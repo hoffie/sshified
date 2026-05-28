33ff8e578e84e442458c946958ea91220a9213a2
Christian Hoffmann <mail@hoffmann-christian.info>
2026-05-28 17:27:28 +0200

    loadtest: add more flexibility for manual testing

--- loadtest/server.go
+++ loadtest/server.go
@@ -5,12 +5,18 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Syntax: server.go PORT\n\n")
+		fmt.Fprintf(os.Stderr, "Error: missing PORT\n")
+		os.Exit(1)
+	}
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Args[1]), nil))
 }
